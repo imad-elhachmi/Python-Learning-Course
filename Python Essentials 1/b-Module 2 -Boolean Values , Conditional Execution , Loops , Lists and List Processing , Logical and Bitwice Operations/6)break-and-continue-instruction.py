6905bd736e201191feb23df5c140c19422849eaf
imad-elhachmi <im.imadelhachmi@gmail.com>
2025-10-04 18:52:12 +0200

    Add files via upload

---
+++ Python Essentials 1/b-Module 2 -Boolean Values , Conditional Execution , Loops , Lists and List Processing , Logical and Bitwice Operations/6)break-and-continue-instruction.py
@@ -0,0 +1,19 @@
+                # the Break statement
+#Stop and exit the loop when a certain condition is met
+print("the Break statment :")
+for i in range(11) :
+    if i == 4 :
+        break
+    print(i)
+print("outside the loop.")
+# output 
+
+            #the continue statement
+#If a specific condition is met, skip the rest of the code in that iteration and start the next one
+j = 0
+while j < 10:
+    if j == 4:
+        j += 1
+        continue
+    print(j)
+    j += 1
