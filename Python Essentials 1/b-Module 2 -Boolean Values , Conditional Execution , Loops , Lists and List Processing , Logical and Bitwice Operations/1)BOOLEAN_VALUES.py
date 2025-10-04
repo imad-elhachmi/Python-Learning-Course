d1776deee7dc95d01b399cdea19592e0479d6946
imad-elhachmi <im.imadelhachmi@gmail.com>
2025-10-04 14:18:21 +0200

    Add files via upload

--- Python Essentials 1/b-Module 2 -Boolean Values , Conditional Execution , Loops , Lists and List Processing , Logical and Bitwice Operations/1)BOOLEAN_VALUES.py
+++ Python Essentials 1/b-Module 2 -Boolean Values , Conditional Execution , Loops , Lists and List Processing , Logical and Bitwice Operations/1)BOOLEAN_VALUES.py
@@ -1,1 +1,3 @@
-
+# computer language : 1 is True and 0 is False
+print(True > False) # True (1 > 0)
+print(True < False) # False (1 < 0)
---
+++ Python Essentials 1/b-Module 2 -Boolean Values , Conditional Execution , Loops , Lists and List Processing , Logical and Bitwice Operations/2)Relation-operators.py
@@ -0,0 +1,18 @@
+# == , != , > , < , >= , <=
+a , b , c = 2 , 2 , 8
+# the relation operator ==
+print(a==b) # True
+print(a==c) # False
+# the relation operator !=
+print(a!=b) # False
+print(a!=c) # True
+# the relation operator > 
+print(a>b) # False
+# the relation operator <
+print(a < b) # False
+# the relation operator >=
+print(a >= b) # True
+print(a >= c) # False
+# the relation operator <=
+print(a <= b) # True
+print(a <= c) # True 
